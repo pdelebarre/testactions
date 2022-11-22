ef328c34bcae06b135456baea4ed3f8968cfd715
Philippe <pdelebarre@gmail.com>
2022-11-22 15:08:18 +0100

    first

---
+++ src/main/java/com/example/testactions/TestactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestactionsApplication.class, args);
+	}
+
+}
